ef09a423580bc06762115a42d48eaa9598fe8c5e
joagon96@gmail.com <joagon96@gmail.com>
2020-08-14 18:25:16 -0300

    cambio conexion

--- Conexion.php
+++ Conexion.php
@@ -1,4 +1,8 @@
 <?php
-$link = mysqli_connect("localhost", "root") or die ("Problemas de conexión a la base de datos");
-mysqli_select_db($link, "tpi_vacantes");
+// $link = mysqli_connect("localhost", "root") or die ("Problemas de conexión a la base de datos");
+// mysqli_select_db($link, "tpi_vacantes");
+
+$link = mysqli_connect("localhost", "id14611645_root", '6Ls$s}@)C\3Y4lEt') or die ("Problemas de conexión a la base de datos");
+mysqli_select_db($link, "id14611645_tpi_vacantes");
+
 ?>
