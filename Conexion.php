53803928f7f3ef010a1f71d4f8e1997835ed64be
Augusto De Lisio <augustodelisio@gmail.com>
2020-09-03 18:02:12 -0300

    Conexion con bd

--- Conexion.php
+++ Conexion.php
@@ -1,8 +1,8 @@
 <?php
-// $link = mysqli_connect("localhost", "root") or die ("Problemas de conexión a la base de datos");
-// mysqli_select_db($link, "tpi_vacantes");
-
-$link = mysqli_connect("localhost", "root", '') or die ("Problemas de conexión a la base de datos");
+$link = mysqli_connect("localhost", "root") or die ("Problemas de conexión a la base de datos");
 mysqli_select_db($link, "tpi_vacantes");
+
+//$link = mysqli_connect("localhost", "id14611645_root", '6Ls$s}@)C\3Y4lEt') or die ("Problemas de conexión a la base de datos");
+//mysqli_select_db($link, "id14611645_tpi_vacantes");
 
 ?>
