4de7ff79b1fb669aa1c98d18112a8e0f6637b36e
Augusto De Lisio <augustodelisio@gmail.com>
2020-09-03 17:58:48 -0300

    cambio iconos por botones Bolsa

--- Conexion.php
+++ Conexion.php
@@ -2,7 +2,7 @@
 // $link = mysqli_connect("localhost", "root") or die ("Problemas de conexión a la base de datos");
 // mysqli_select_db($link, "tpi_vacantes");
 
-$link = mysqli_connect("localhost", "id14611645_root", '6Ls$s}@)C\3Y4lEt') or die ("Problemas de conexión a la base de datos");
-mysqli_select_db($link, "id14611645_tpi_vacantes");
+$link = mysqli_connect("localhost", "root", '') or die ("Problemas de conexión a la base de datos");
+mysqli_select_db($link, "tpi_vacantes");
 
 ?>
--- CubrirVacantes.php
+++ CubrirVacantes.php
@@ -90,37 +90,47 @@ if (isset($_SESSION['usuario'])){
                 if (isset($tipo)){
                     if ($hoy < $fecha or $tipo == "admin"){
                     ?>
-                        <a class="btn primary" href="Detalles.php?var=<?php echo $var?>">                                                                <!--  en el href le agregue la variable que cree antes para mandarla a esa direccion -->
+                        <a class="btn primary" href="Detalles.php?var=<?php echo $var?>">
+                        <!--  en el href le agregue la variable que cree antes para mandarla a esa direccion -->
+                        <button type="button" class="btn btn-primary">
                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
-                        <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-                        </svg></a>
+                            <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+                            <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+                        </svg>
+                        Inscribir Nuevo </button></a>
                     <?php    
                     }else{
                     ?>
                         <a class="btn disabled" href="Detalles.php">
+                        <button type="button" class="btn btn-secondary">
                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
                         <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-                        </svg></a>
+                        </svg>
+                        Vacante Cerrada </button></a>
                         <?php    
                     }
                 }else{
                     if ($hoy < $fecha ){
                         ?>
-                            <a class="btn primary" href="Detalles.php?var=<?php echo $var?>">                                                                <!--  en el href le agregue la variable que cree antes para mandarla a esa direccion -->
+                            <a class="btn primary" href="Detalles.php?var=<?php echo $var?>">
+                            <!--  en el href le agregue la variable que cree antes para mandarla a esa direccion -->
+                            <button type="button" class="btn btn-primary">
                             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
                             <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-                            </svg></a>
+                            </svg>
+                            Inscribir Nuevo </button></a>
                         <?php    
                         }else{
                         ?>
                             <a class="btn disabled" href="Detalles.php">
+                            <button type="button" class="btn btn-secondary">
                             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
                             <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-                            </svg></a>
+                            </svg>
+                            Vacante Cerrada </button></a>
                             <?php    
                         }
                 }
@@ -134,33 +144,45 @@ if (isset($_SESSION['usuario'])){
                 if (isset($tipo)){
                     if ($hoy >= $fecha or $tipo == "admin"){
                     ?>
-                        <a class="btn primary" href="Merito.php?var=<?php echo $var?>" ><svg width="1.3em" height="1.3em" viewBox="0 0 16 16" class="bi bi-file-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <a class="btn primary" href="Merito.php?var=<?php echo $var?>">
+                        <button type="button" class="btn btn-primary">
+                        <svg width="1.3em" height="1.3em" viewBox="0 0 16 16" class="bi bi-file-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M10.854 6.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 8.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
                         <path fill-rule="evenodd" d="M4 1h8a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H4z"/>
-                        </svg></a>
+                        </svg>
+                        Ver Resultados </button></a>
                     <?php    
                     }else{
                     ?>
-                        <a class="btn disabled" href="Merito.php" ><svg width="1.3em" height="1.3em" viewBox="0 0 16 16" class="bi bi-file-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <a class="btn disabled" href="Merito.php">
+                        <button type="button" class="btn btn-secondary">
+                        <svg width="1.3em" height="1.3em" viewBox="0 0 16 16" class="bi bi-file-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M10.854 6.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 8.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
                         <path fill-rule="evenodd" d="M4 1h8a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H4z"/>
-                        </svg></a>
+                        </svg>
+                        No Disponible </button></a>
                     <?php    
                     }
                 }else{
                     if ($hoy >= $fecha){
                         ?>
-                            <a class="btn primary" href="Merito.php?var=<?php echo $var?>" ><svg width="1.3em" height="1.3em" viewBox="0 0 16 16" class="bi bi-file-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <a class="btn primary" href="Merito.php?var=<?php echo $var?>">
+                            <button type="button" class="btn btn-primary">
+                            <svg width="1.3em" height="1.3em" viewBox="0 0 16 16" class="bi bi-file-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M10.854 6.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 8.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
                             <path fill-rule="evenodd" d="M4 1h8a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H4z"/>
-                            </svg></a>
+                            </svg>
+                            Ver Resultados </button></a>
                         <?php    
                         }else{
                         ?>
-                            <a class="btn disabled" href="Merito.php" ><svg width="1.3em" height="1.3em" viewBox="0 0 16 16" class="bi bi-file-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <a class="btn disabled" href="Merito.php">
+                            <button type="button" class="btn btn-secondary">
+                            <svg width="1.3em" height="1.3em" viewBox="0 0 16 16" class="bi bi-file-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M10.854 6.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 8.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
                             <path fill-rule="evenodd" d="M4 1h8a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H4z"/>
-                            </svg></a>
+                            </svg>
+                            No Disponible </button></a>
                         <?php    
                         }
                     }
