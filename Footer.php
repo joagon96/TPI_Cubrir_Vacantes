bc69c7f6fc9c983c291aff84b9daac1f3a78bd9d
joagon96@gmail.com <joagon96@gmail.com>
2020-09-07 21:23:58 -0300

    final commit v2

--- Footer.php
+++ Footer.php
@@ -1,4 +1,4 @@
 <footer class="container">
         <br>
-        <p>&copy; UTN FRRo 2020 - Contacto:<a href="Contacto.php"> jcpistol16@gmail.com </a> TEL:341789465 </p>
+        <p class="text-center">&copy; UTN FRRo 2020 - Contacto:<a href="Contacto.php">  jcpistol16@gmail.com </a> - TEL:341789465 - <a href="MapaDelSitio.php">Mapa Funcional</a> </p>
 </footer>
