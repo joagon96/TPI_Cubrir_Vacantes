be897de4b3064dc8c6f58fc05a774d076ae6e917
Augusto De Lisio <augustodelisio@gmail.com>
2020-09-12 19:17:31 -0300

    Mapa del sitio

--- Footer.php
+++ Footer.php
@@ -1,4 +1,22 @@
 <footer class="container">
         <br>
-        <p class="text-center">&copy; UTN FRRo 2020 - Contacto:<a href="Contacto.php">  jcpistol16@gmail.com </a> - TEL:341789465 </p>
+        <p class="text-center">&copy; UTN FRRo 2020 - Contacto:<a href="Contacto.php"> jcpistol16@gmail.com </a> - TEL:341789465 </p>
+        <div class="text-left" style="background-color: whitesmoke; width: 100%; padding: 10px 0 20px 0;">
+                <ul style="list-style-type: none;">
+                        <li><a href="Home.php">Inicio</a></li>
+                        <li><a href="CubrirVacantes.php">Bolsa de trabajo</a></li>
+                        <li><a href="Contacto.php">Contacto</a></li>
+                        <?php
+                        if (isset($_SESSION['usuario'])) {
+                                $tipo = $_SESSION['tipo'];
+
+                                if ($tipo != "") {
+                                        ?>
+                                        <li><a href="Merito.php"></a></li>
+                                        <?php
+                                }
+                        }
+                        ?>
+                </ul>
+        </div>
 </footer>
